157c7b77f826f5dbd6e0a5f2e53713c32241b55e
edus44 <eduhhg@gmail.com>
2017-01-17 12:14:10 +0100

    added node subconfig

---
+++ node.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env":{
+        "node":true,
+        "mocha":true,
+        "es6":true,
+        "commonjs":true
+    },
+    "parserOptions": {
+        "ecmaVersion" : 7,
+        "sourceType": "script"
+    },
+    'extends': [
+        './rules/best-practices',
+        './rules/errors',
+        './rules/es6',
+        './rules/node',
+        './rules/strict',
+        './rules/style',
+        './rules/variables',
+        './rules/promise',
+    ].map(require.resolve),
+    'rules':{
+        'strict' : [2,'global']
+    }
+};
